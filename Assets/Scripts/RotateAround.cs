87fc420c6d3a08a6c68bedc9680935b5e964fdb2
Cabbage <kiwibirdi@hotmail.com>
2020-02-01 10:16:35 +0100

    rotation

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RotateAround : MonoBehaviour
 {
     public GameObject OrbitPoint_;
+    private GameObject HammerMan_;
+    private Rigidbody2D HammerBody_;
+    public Transform parent_;
 
     public float orbitDistance_;
     [Header("Speed variables")]
@@ -12,28 +15,92 @@ public class RotateAround : MonoBehaviour
     public float speedIncrease_;
     [Tooltip("start speed")]
     public float orbitSpeed_;
-    private float orbit_;
-    Vector3 tempPos_;
+    [Tooltip("orbit speed cap")]
+    public float orbitSpeedCap_;
+    [Tooltip("Force multiplier when releasing hammer")]
+    public float forceMultiplier_;
+    private float orbit_; //radian degree toward the center object
+    private float speedReset_;
+    private Vector3 tempPos_;
+    private Vector3 releaseDirection_;
 
     void Start()
     {
+        HammerMan_ = GameObject.FindGameObjectWithTag("Player");
+        HammerBody_ = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         tempPos_ = new Vector3(0, 0, 0);
+        speedReset_ = orbitSpeed_;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            orbit_ -= orbitSpeed_ * Time.deltaTime / 10;
+            tempPos_.x = OrbitPoint_.transform.position.x + Mathf.Cos(orbit_) * orbitDistance_;
+            tempPos_.y = OrbitPoint_.transform.position.y + Mathf.Sin(orbit_) * orbitDistance_;
+            transform.position = tempPos_;
+            HammerMan_.transform.position = transform.position;
+            if (orbitSpeed_ < orbitSpeedCap_)
+                orbitSpeed_ += speedIncrease_;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             orbit_ += orbitSpeed_ * Time.deltaTime / 10;
             tempPos_.x = OrbitPoint_.transform.position.x + Mathf.Cos(orbit_) * orbitDistance_;
             tempPos_.y = OrbitPoint_.transform.position.y + Mathf.Sin(orbit_) * orbitDistance_;
             transform.position = tempPos_;
-            orbitSpeed_ += speedIncrease_;
+            HammerMan_.transform.position = transform.position;
+            if (orbitSpeed_ < orbitSpeedCap_)
+                orbitSpeed_ += speedIncrease_;
         }
-        else //Yeet that hammer
+        else if (Input.GetKeyUp(KeyCode.RightArrow))//Yeet that hammer
         {
-           
+            resetSwing();
+
+            releaseDirection_ = calculateTan(parent_.position, HammerBody_.transform.position);
+            float magnitude = releaseDirection_.magnitude;
+            releaseDirection_ = releaseDirection_ / magnitude;
+            releaseDirection_ *= -1;
+            HammerBody_.AddForce(releaseDirection_ * forceMultiplier_);
+            
         }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            resetSwing();
+
+            releaseDirection_ = calculateTan(parent_.position, HammerBody_.transform.position);
+            float magnitude = releaseDirection_.magnitude;
+            releaseDirection_ = releaseDirection_ / magnitude;
+            HammerBody_.AddForce(releaseDirection_ * forceMultiplier_);
+        }
+    }
+    Vector3 calculateTan(Vector3 lhs, Vector3 rhs)
+    {
+        Vector3 normal_ = rhs - lhs;
+        Vector3 tangent;
+        Vector3 t1 = Vector3.Cross(normal_, Vector3.forward);
+        Vector3 t2 = Vector3.Cross(normal_, Vector3.up);
+        if (t1.magnitude > t2.magnitude)
+        {
+            tangent = t1;
+        }
+        else
+        {
+            tangent = t2;
+        }
+        return tangent;
+    }
+
+    void resetSwing()
+    {
+        HammerBody_.velocity = Vector3.zero;
+        orbitSpeed_ = speedReset_;
     }
 }
