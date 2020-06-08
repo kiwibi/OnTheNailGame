91d06d0faf8c9a34f82cccb617eeac81ac674a46
Simon <simon.ostman.6384@student.uu.se>
2020-06-08 21:45:04 +0200

    Moved everything to NewProject

---
+++ NewProject/OnTheNail/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject objectA;
+    public GameObject objectB;
+
+    void Update()
+    {
+        camera.GetComponent<CameraScript>().SetHammerPosition(transform.position);
+
+        
+    }
+}
